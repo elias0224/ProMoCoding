8f9bdfaef56014b099ca354784207057fdc2ca83
Elias Barth <elias@barthweb.com>
2024-04-18 16:40:07 +0200

    Correct Solution without the usage of init, last

--- baby-steps-with-list.hs
+++ baby-steps-with-list.hs
@@ -3,16 +3,16 @@ kopf :: String -> Char
 kopf s = s!!0
 
 ende :: String -> Char
-ende c = last c
+ende c = kopf(reverse c)
 
 rest :: String -> String
-rest xs = reverse(init (reverse xs))
+rest xs = reverse(take (length xs -1)(reverse xs))
 
 start :: String -> String
-start xs = reverse(take 1 (reverse xs))
+start xs = reverse(drop 1 (reverse xs))
 
 rest2 :: String -> String
 rest2 xs = drop 1 xs
 
 start2 :: String -> String
-start2 xs = init xs
+start2 xs = take (length xs -1) xs
