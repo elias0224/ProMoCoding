6dec1a0d7e530c2f57aa907195bea6268a6807fd
Elias Barth <elias@barthweb.com>
2024-06-18 15:45:27 +0200

    added exercise 3)b)

--- u10.3.hs
+++ u10.3.hs
@@ -1,4 +1,5 @@
 import System.IO
+import Distribution.Simple.Utils (xargs)
 
 main :: IO()
 main = do
@@ -13,8 +14,28 @@ main = do
     if  null translation
         then putStrLn word
         else putStr $ unlines translation
-    
 
+--b)
+-- main :: IO()
+-- main = do
+--     hSetBuffering stdout NoBuffering
+--     contentsWB <- readFile "u10.woerterbuch.txt"
+--     let dict = map read (lines contentsWB)
+
+--     original <- readFile "orgiinal.txt"
+--     putStrln $ unword $ map (look dict) (words original)
+
+-- let translation = dictLookup dict word
+--     if  null translation
+--         then putStrLn word
+--         else putStr $ unlines translation
+
+
+
+-- look :: [(Sring, String)] -> String -> String
+-- look dict word = case dictLookup dict word of 
+--     (x:xs) -> xs 
+--      _     -> word 
 
 dictLookup :: [(String, String)] -> String -> [String]
 dictLookup dict word = map snd $ filter ((==word) . fst) dict
